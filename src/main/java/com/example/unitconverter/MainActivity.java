92b2d0d321d02f687bf386b2b7200155f159ad7a
Byron <byronhe98@gmail.com>
2020-02-13 01:35:22 -0600

    updated src folder
    all files in src in project

--- src/main/java/com/example/unitconverter/MainActivity.java
+++ src/main/java/com/example/unitconverter/MainActivity.java
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         txtF = findViewById(R.id.txtF);
         txtC = findViewById(R.id.txtC);
+        txtMiles = findViewById(R.id.txtMiles);
+        txtKilometers = findViewById(R.id.txtKilometers);
+        txtPounds = findViewById(R.id.txtPounds);
+        txtKilograms = findViewById(R.id.txtKilograms);
+        txtGallons = findViewById(R.id.txtGallons);
+        txtLiters = findViewById(R.id.txtLiters);
 
 
         txtF.addTextChangedListener(new TextWatcher() {
@@ -29,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
 
             @Override
-            public void afterTextChanged(Editable s) {
+            public void afterTextChanged(Editable s) {// convert F to C
                 String strF = s.toString();
                 Log.i("UnitConverter", "Value in txtF = " + strF);
 
@@ -59,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
 
             @Override
-            public void afterTextChanged(Editable s) {
+            public void afterTextChanged(Editable s) { //convert C to F
                 String strC = s.toString();
                 Log.i("UnitConverter", "Value in txtC = " + strC);
 
@@ -92,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void afterTextChanged(Editable s) {
+            public void afterTextChanged(Editable s) { //convert miles to kilometers
                 String strMiles = s.toString();
                 Log.i("UnitConverter", "Value in txtMiles = " + strMiles);
 
@@ -107,6 +113,171 @@ public class MainActivity extends AppCompatActivity {
 
                     MainActivity.this.txtKilometers.setText(strKilometers);
                 }  catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        txtKilometers.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {  //convert kilometers to miles
+                String strKilometers = s.toString();
+                Log.i("UnitConverter", "Value in txtKilometers = " + strKilometers);
+
+                if (!txtKilometers.isFocused()) return;
+                if (strKilometers.length() == 0) return;
+
+                try {
+                    double valKilometers = Double.parseDouble(strKilometers);
+                    double valMiles = valKilometers/1.6093;
+                    String strMiles = Double.toString(valMiles);
+                    Log.i("UnitConverter", "Value in txtMiles = " + strMiles);
+
+                    MainActivity.this.txtMiles.setText(strMiles);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        txtPounds.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { //convert pounds to kilograms
+                String strPounds = s.toString();
+                Log.i("UnitConverter", "Value in txtPounds = " + strPounds);
+
+                if (!txtPounds.isFocused()) return;
+                if (strPounds.length() == 0) return;
+
+                try {
+                    double valPounds = Double.parseDouble(strPounds);
+                    double valKilograms = valPounds/1.6093;
+                    String strKilograms = Double.toString(valKilograms);
+                    Log.i("UnitConverter", "Value in txtKilograms = " + strKilograms);
+
+                    MainActivity.this.txtKilograms.setText(strKilograms);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        txtKilograms.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { //convert kilograms to pounds
+                String strKilograms = s.toString();
+                Log.i("UnitConverter", "Value in txtKilograms = " + strKilograms);
+
+                if (!txtKilograms.isFocused()) return;
+                if (strKilograms.length() == 0) return;
+
+                try {
+                    double valKilograms = Double.parseDouble(strKilograms);
+                    double valPounds = valKilograms/1.6093;
+                    String strPounds = Double.toString(valPounds);
+                    Log.i("UnitConverter", "Value in txtPounds = " + strPounds);
+
+                    MainActivity.this.txtPounds.setText(strPounds);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        txtGallons.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { //convert gallons to liters
+                String strGallons = s.toString();
+                Log.i("UnitConverter", "Value in txtGallons = " + strGallons);
+
+                if (!txtGallons.isFocused()) return;
+                if (strGallons.length() == 0) return;
+
+                try {
+                    double valGallons = Double.parseDouble(strGallons);
+                    double valLiters = valGallons*3.7854;
+                    String strLiters = Double.toString(valLiters);
+                    Log.i("UnitConverter", "Value in txtLiters = " + strLiters);
+
+                    MainActivity.this.txtLiters.setText(strLiters);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        txtLiters.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { //convert liters to gallons
+                String strLiters = s.toString();
+                Log.i("UnitConverter", "Value in txtLiters = " + strLiters);
+
+                if (!txtLiters.isFocused()) return;
+                if (strLiters.length() == 0) return;
+
+                try {
+                    double valLiters = Double.parseDouble(strLiters);
+                    double valGallons = valLiters/3.7854;
+                    String strGallons = Double.toString(valGallons);
+                    Log.i("UnitConverter", "Value in txtGallons = " + strGallons);
+
+                    MainActivity.this.txtGallons.setText(strGallons);
+
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
