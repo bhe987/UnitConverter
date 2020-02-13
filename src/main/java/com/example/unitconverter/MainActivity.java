27a436d03ec432651edc3b6615e41b0a71954ac9
Byron <byronhe98@gmail.com>
2020-02-12 23:51:17 -0600

    Add src file
    Added src file from the app folder in the UnitConverter project from the folder AndroidStudioProjects. This is for now until I can set up git to upload everything.

---
+++ src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText txtF, txtC, txtMiles, txtKilometers, txtPounds, txtKilograms, txtGallons, txtLiters;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtF = findViewById(R.id.txtF);
+        txtC = findViewById(R.id.txtC);
+
+
+        txtF.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String strF = s.toString();
+                Log.i("UnitConverter", "Value in txtF = " + strF);
+
+                if (!txtF.isFocused()) return;
+                if (strF.length() == 0) return;
+
+
+                try {
+                    double valF = Double.parseDouble(strF);
+                    double valC = (valF - 32.0)*5/9;
+                    String strC = Double.toString(valC);
+                    Log.i("UnitConverter", "Value in txtC = " + strC);
+
+                    MainActivity.this.txtC.setText(strC);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } );
+
+        txtC.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String strC = s.toString();
+                Log.i("UnitConverter", "Value in txtC = " + strC);
+
+                if (!txtC.isFocused()) return;
+                if (strC.length() == 0) return;
+
+                try {
+                    double valC = Double.parseDouble(strC);
+                    double valF = valC*9/5 + 32;
+                    String strF = Double.toString(valF);
+                    Log.i("UnitConverter", "Value in txtF = " + strF);
+
+                    MainActivity.this.txtF.setText(strF);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } );
+
+        txtMiles.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String strMiles = s.toString();
+                Log.i("UnitConverter", "Value in txtMiles = " + strMiles);
+
+                if (!txtMiles.isFocused()) return;
+                if (strMiles.length() == 0) return;
+
+                try {
+                    double valMiles = Double.parseDouble(strMiles);
+                    double valKilometers = valMiles * 1.6093;
+                    String strKilometers = Double.toString(valKilometers);
+                    Log.i("UnitConverter", "Value in txtKilometers = " + strKilometers);
+
+                    MainActivity.this.txtKilometers.setText(strKilometers);
+                }  catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
